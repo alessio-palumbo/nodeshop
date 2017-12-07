41f60fdc6079f89b90d3a51c17e71a40ae5bc863
Alessio-Palumbo <alessio.palumbo4@gmail.com>
2017-12-07 12:28:53 +1100

    Display user information when sign in

--- web/src/App.js
+++ web/src/App.js
@@ -3,28 +3,59 @@ import './bootstrap-4.0.0-beta.2-dist/css/bootstrap.min.css'
 import './App.css';
 import SignInForm from './components/SignInForm'
 import { signIn } from './api/auth'
+import { listProducts } from './api/products'
+import { setToken } from './api/init'
 
 class App extends Component {
+  state = {
+    decodeToken: null
+  }
+
   onSignIn = ({ email, password }) => {
     console.log('App received', { email, password })
     signIn({ email, password })
-      .then(data => {
-        console.log('signed in', data)
+      .then(decodedToken => {
+        console.log('signed in', decodedToken)
+        this.setState({ decodedToken })
       })
   }
 
   render() {
+    const { decodedToken } = this.state
+
     return (
       <div className="App">
         <div className='jumbotron'>
           <h1 className='text-center'>Yarra</h1>
           <h4>Now Delivering: Shipping trillions of new products</h4>
         </div>
-        <SignInForm
-          onSignIn={this.onSignIn}
-        />
+        {
+          decodedToken ? (
+            <div>
+              <p>Email: {decodedToken.email}</p>
+              <p>Signed in: {new Date(decodedToken.iat * 1000).toISOString()}</p>
+              <p>Expire in: {new Date(decodedToken.exp * 1000).toISOString()}</p>
+            </div>
+
+          ) : (
+              <SignInForm
+                onSignIn={this.onSignIn}
+              />
+            )
+        }
       </div>
     );
+  }
+
+  // When this App first appear on screen
+  componentDidMount() {
+    listProducts()
+      .then(products => {
+        console.log(products)
+      })
+      .catch(error => {
+        console.error('error loading products', error)
+      })
   }
 }
 
